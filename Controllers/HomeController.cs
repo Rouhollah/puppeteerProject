005475dae3f3da7c685ac6f8faa795775080edf3
Rouhollah <torshizi1@gmail.com>
2020-07-09 12:06:30 +0430

    change project name and pre init

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Razor.Language;
 using PuppeteerSharp;
 
 [Route("api/[controller]")]
@@ -8,10 +10,10 @@ using PuppeteerSharp;
 public class HomeController : Controller
 {
     [HttpPost("[action]")]
-    public async Task<SearchInUrl> google(SearchInUrl input)
+    public async Task<List<SearchInUrl>> google(SearchInUrl input)
     {
         var searchKey = input.Url + "&tbm=isch&tbs=isz%3Al&hl=en&biw=1349&bih=663";
-        var searchInUrl = new SearchInUrl();
+        List<SearchInUrl> searchInUrl = new List<SearchInUrl>();
         var browser = await Puppeteer.LaunchAsync(new LaunchOptions
         {
             Headless = false,
@@ -20,17 +22,67 @@ public class HomeController : Controller
         using (var page = await browser.NewPageAsync())
         {
             await page.GoToAsync(searchKey);
-            var jsSelectAllAnchors = @"Array.from(document.querySelectorAll('a')).map(a => { 
-                if(a.href.includes(imgres?imgurl){
-                    return a.href;
-            }
-            })";
+            List<string> allLink = new List<string>();
+            // var jsSelectLengthString = @"(document.querySelectorAll('#islrg > div.islrc > div').length)";
+            var jsSelectAllAnchors = @"Array.from(document.querySelectorAll('#islrg > div.islrc > div >a.wXeWr.islib.nfEiy.mM5pbd')).map(a => a.href)";
+             //{ 
+             //    if(a.href.includes(imgres?imgurl){
+             //        return a.href;
+             //}
+             //})";
             var urls = await page.EvaluateExpressionAsync<string[]>(jsSelectAllAnchors);
-            foreach (string url in urls)
+
+            //int jsSelectLength= await page.EvaluateExpressionAsync<int>(jsSelectLengthString);
+            //for (int i = 0; i < jsSelectLength; i++) {
+            //    i++;
+            //   var  linkString = @"document.querySelectorAll('#islrg > div.islrc > div:nth-child("+ i +") > a.wXeWr.islib.nfEiy.mM5pbd')";
+            //    Console.WriteLine(linkString);
+            //    var tag = await page.EvaluateExpressionAsync<string>(linkString);
+            //    Console.WriteLine(tag);
+            //    allLink.Add(tag);
+
+            //   // var one = await page.QuerySelectorAllHandleAsync("#islrg > div.islrc > div:nth-child(" + i + ") > a.wXeWr.islib.nfEiy.mM5pbd")
+            //   //.EvaluateFunctionAsync<string[]>("elements => elements.map(a=>a['href'])");
+            //   // Console.WriteLine(one);
+            //    i--;
+            //}
+            // var jsSelectAll = @"Array.from(document.querySelectorAll('#islrg > div.islrc > div').length)";
+            // var countLink = await page.QuerySelectorAllHandleAsync("#islrg > div.islrc > div").EvaluateFunctionAsync<string[]>("" +
+            //    "(elements)=>{" +
+            //    "document.querySelectorAll;}" +
+            //    "");
+               // .EvaluateFunctionAsync<string[]>("elements => elements.map(a => a.href)");
+            //for (int i = 0; i < countLink.Length; i++)
+            //{
+            //    var href = await page.QuerySelectorAllHandleAsync("#islrg > div.islrc > div:nth-child(" + i + 1 + ") > a.wXeWr.islib.nfEiy.mM5pbd")
+            //        .EvaluateFunctionAsync<string[]>("elements => {console.log(elements);}");
+            //}
+            var allLinks = await page.QuerySelectorAllHandleAsync("#yDmH0d > div.T1diZc.KWE8qe > c-wiz > div.mJxzWe a")
+                .EvaluateFunctionAsync<string[]>("elements => elements.map(a => a.href)");
+
+           
+            foreach (var item in allLinks)
             {
-                Console.WriteLine(url);
-                await page.GoToAsync(url);
+                SearchInUrl url = new SearchInUrl();
+                url.Url = item.Trim();
+                searchInUrl.Add(url);
+                //var url = item.ExecutionContext.Frame.Url;
+                //if (url.Contains("imgres?imgurl"))
+                //{
+                //    await page.GoToAsync(url);
+                //}
             }
+            // var jsSelectAllAnchors = @"Array.from(document.querySelectorAll('a')).map(a => { 
+            //     if(a.href.includes(imgres?imgurl){
+            //         return a.href;
+            // }
+            // })";
+            // var urls = await page.EvaluateExpressionAsync<string[]>(jsSelectAllAnchors);
+            // foreach (string url in urls)
+            // {
+            //     Console.WriteLine(url);
+            //     await page.GoToAsync(url);
+            // }
         }
         return searchInUrl;
     }
